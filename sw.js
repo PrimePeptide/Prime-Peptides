6af9ea5d114df005f2c82f634822ed42586ae141
PrimePeptide <gw2g56mwz6@privaterelay.appleid.com>
2026-09-07 09:57:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='prime-protocol-v2';
+const CACHE='prime-protocol-v3';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{let copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}))));
